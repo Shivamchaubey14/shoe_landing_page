30a677b03238a04a50af88ed683636bb2b814646
Shivam <shivamc36@gmail.com>
2024-11-09 14:40:44 +0530

    Added all the neccessary components

---
+++ src/sections/index.js
@@ -0,0 +1,23 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+
+import Subscribe from "./Subscribe";
+
+import SuperQuality from "./SuperQuality";
+
+import PopularProducts from "./PopularProducts";
+
+
+export {
+    Hero,
+    Footer,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    PopularProducts,
+    CustomerReviews
+}
