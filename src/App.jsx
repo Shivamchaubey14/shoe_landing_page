06f253ee206bbf6409eb4b9e61bdfc44f909a085
Shivam <shivamc36@gmail.com>
2024-11-09 14:28:09 +0530

    Created a sections for landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-export default function App() {
+import React from 'react'
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello World
-    </h1>
+    <main className='relative'>
+      Nav
+      <section className='xl:padding-1 wide:padding-b'>
+        Hero
+      </section>
+      <section className='padding'>
+        PropularProducts
+      </section>
+      <section className='padding'>
+        SuperQuality
+      </section>
+      <section className='padding-x py-10'>
+        Services
+      </section>
+      <section className='padding'>
+        SpecialOffer
+      </section>
+      <section className='padding bg-pale-blue'>
+        CustomerReviews
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        Subscribe
+      </section>
+      <section className='padding-x bg-black padding-t pb-8'>
+        Footer
+      </section>
+    </main>
   )
 }
+
+export default App
